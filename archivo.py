38b9a3a2b92d254f81de0eba638567e5fcb3c19e
Andres <andreslenis280@gmail.com>
2024-07-27 19:22:06 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -1,4 +1,4 @@
-nombre = 'Mateo' # agregar nombre
+nombre = 'Camila' # agregar nombre
 saludo = 'Que deseas hacer hoy' # agregar un saludo o una frase 
 segundo_nombre = 'Davies' # agregar segundo nombre o apellido
 print('Hola' + " " + nombre + " " + segundo_nombre + " " + saludo)
